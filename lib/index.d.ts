aedd557e18f1b525c4d17b1512f440954162e963
peterkarrer <peter@karrer.pro>
2017-06-06 15:52:04 +0200

    removed unnecessary compiled version and changed package.json accordingly

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Enum {
-    static fromArray<T extends string>(o: Array<T>): {
-        [K in T]: K;
-    };
-    static fromObject<T extends {
-        [_: string]: V;
-    }, V extends string>(definition: T): T;
-}
